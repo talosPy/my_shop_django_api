d186e512454dac8520fb2616726d576c04d7130d
Ran Erlich <ranerlich@gmail.com>
2024-09-24 19:47:58 +0300

    added single product endpoint

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ from products import views
 
 urlpatterns = [
     path('', views.product_list, name="product_list"),
+    path('<int:id>/', views.product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializer
+from rest_framework.generics import get_object_or_404
 
 
 @api_view(['GET', 'POST'])
@@ -22,3 +23,26 @@ def product_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, id):
+    """
+    Retrieve, update or delete a code product.
+    """
+    product = get_object_or_404(Product, id=id)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
