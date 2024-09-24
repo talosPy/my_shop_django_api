82fc59e41a8dcf0ec8a58e5f2b3df31cc9412b2a
Ran Erlich <ranerlich@gmail.com>
2024-09-24 18:30:12 +0300

    added rest framework GET + POST products

--- products/views.py
+++ products/views.py
@@ -1,9 +1,24 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializer
 
+
+@api_view(['GET', 'POST'])
 def product_list(request):
-    all_products = ProductSerializer(Product.objects.all(), many=True).data
-    return JsonResponse(all_products, safe=False)
-    
+    """
+    List all  products, or create a new product.
+    """
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        print("json recieved is:", request.data)
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
