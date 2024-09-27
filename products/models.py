7b60d6395b2c97d77549f68ffdd4c7057ed333a8
Ran Erlich <ranerlich@gmail.com>
2024-09-27 12:59:05 +0300

    added images

--- products/models.py
+++ products/models.py
@@ -12,6 +12,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ManyToManyField(Category,related_name="products")
+    image = models.ImageField(null=True,blank=True,default='/placeholder.png')
+
 
     def __str__(self):
         return f"{self.name} - {self.price}"
