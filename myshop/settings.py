a38a967120f6cc151ba136750e5cedf4aa9f22b7
talosPy <tala0147@gmail.com>
2024-10-08 18:17:09 +0300

    added security

--- myshop/settings.py
+++ myshop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v*e5rqnvdh++&xh@-oy^*bz!2=k+3s^*#aa9f^qm$f&tu$g@^7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
